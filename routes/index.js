d86fbfe4f0a37924f321b57a57215de5ad6a4d02
Kunwar Gautam <gautam@thinkroman.com>
2023-06-21 10:31:59 -0500

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var router = express.Router()
 
+const databaseUrl = process.env.DATABASE_URL
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', {title: 'Express', url: process.env.DATABASE_URL})
+	res.render('index', {title: 'Express', url: databaseUrl ?? 'No url'})
 })
 
 module.exports = router
